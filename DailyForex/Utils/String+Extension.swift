593b78b39d12ba07452555af84170184a9195c4c
jithin <jithinsyamms@gmail.com>
2022-01-03 12:32:56 +0530

    Added localization support

---
+++ DailyForex/Utils/String+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  String+Extension.swift
+//  DailyForex
+//
+//  Created by Jithin on 03/01/22.
+//
+
+import Foundation
+
+extension String{
+    
+    var localized:String{
+        NSLocalizedString(self, comment: "")
+    }
+    
+    func localized(comment:String) -> String{
+        return NSLocalizedString(self, comment: comment)
+    }
+}
